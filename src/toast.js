262d1e4e06c6d2a873272d4b74506ea43f153e9b
daseul-jang <jds940823@naver.com>
2022-04-07 12:14:04 +0900

    toast 팝업을 위한 sweetalert2 추가

---
+++ src/toast.js
@@ -0,0 +1,21 @@
+import Swal from "sweetalert2";
+
+const config = Swal.mixin({
+  toast: true,
+  position: "center",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: false,
+  showCloseButton: true,
+});
+
+const success = (msg) => {
+  config.fire({
+    icon: "success",
+    title: `${msg}되었습니다.`
+  })
+};
+
+export const Toast = {
+  success,
+};
