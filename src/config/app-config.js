aa031c3d305cde6e2cb6beda15ad2d1e90aadfa0
daseul-jang <jds940823@naver.com>
2022-04-13 02:20:04 +0900

    .env 파일 추가, 백엔드 서버 주소 위치 변경

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -3,7 +3,7 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
   // 백엔드 서버 포트 (톰캣)
-  backendHost = "http://localhost:8090";
+  backendHost = process.env.REACT_APP_BASE_URL;
 }
 
 export const API_BASE_URL = `${backendHost}`;
