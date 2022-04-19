65ba2e7bfcd5031879e4baab966fc65d6e04c6ad
daseul-jang <jds940823@naver.com>
2022-04-19 21:07:17 +0900

    백엔드 서버 url 변경

--- src/config/app-config.js
+++ src/config/app-config.js
@@ -3,7 +3,9 @@ const hostname = window && window.location && window.location.hostname;
 
 if (hostname === "localhost") {
   // 백엔드 서버 포트 (톰캣)
-  backendHost = process.env.REACT_APP_BASE_URL;
+  backendHost = process.env.REACT_APP_LOCALHOST;
+} else {
+  backendHost = process.env.REACT_APP_AWS_URL;
 }
 
 export const API_BASE_URL = `${backendHost}`;
